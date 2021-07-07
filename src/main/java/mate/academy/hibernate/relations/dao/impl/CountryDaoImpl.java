3132362cca5e0835d874c716a58bfd4c74c219ba
rkrylov <Cate77dila>
2021-07-07 17:25:18 +0300

    refactored (Next time it won't be, sorry)

--- src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
+++ src/main/java/mate/academy/hibernate/relations/dao/impl/CountryDaoImpl.java
@@ -2,7 +2,6 @@ package mate.academy.hibernate.relations.dao.impl;
 
 import mate.academy.hibernate.relations.dao.CountryDao;
 import mate.academy.hibernate.relations.model.Country;
-import mate.academy.hibernate.relations.model.Movie;
 import mate.academy.hibernate.relations.util.DataProcessingException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
